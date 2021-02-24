922e8888ec9d2b75f821360c66792a7004c6cff5
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-02-24 16:10:55 +0100

    [UPDATE] In random_cards.js function getPokerdeck

--- assets/scripts/random_cards.js
+++ assets/scripts/random_cards.js
@@ -1,29 +1,21 @@
-function startRandom() {
-    cardsHand = remainingDeck.slice(0, 5);
-    cardsHand.forEach(card => console.log(card));
-    getRemainingDeck(remainingDeck);
-    printDeck(remainingDeck); 
-    var randomCardsReadyForEvaluation = cardsHand.map(figuresIntoIntegers).sort((a, b) => a - b);
-    console.log("----------------------");
-    console.log(randomCardsReadyForEvaluation);
-    console.log("----------------------");
-}
+// function startRandom() {
+    
+//     console.log("----------------------");
+//     console.log(randomCardsReadyForEvaluation);
+//     console.log("----------------------");
+// }
 
-function displayRandomCards() {
-    printDeck(getPokerDeck());
-    console.log("---------------------")
-    var cardsDeck = getPokerDeck();
-    shuffle(cardsDeck);
-    printDeck(cardsDeck);
-    cardsHand = cardsDeck.slice(0, 5);
-    cardsHand.forEach(card => console.log(card));
-    remainingDeck = getRemainingDeck(cardsDeck);
-    printDeck(remainingDeck);
-    var randomCardsReadyForEvaluation = cardsHand.map(figuresIntoIntegers).sort((a, b) => a - b);
-    console.log("----------------------");
-    console.log(randomCardsReadyForEvaluation);
-    console.log("----------------------");
-}
+// function displayRandomCards() {
+//     printDeck(getPokerDeck());
+//     console.log("---------------------")
+//     var cardsDeck = getPokerDeck();
+//     shuffle(cardsDeck);
+//     printDeck(cardsDeck);
+    
+//     console.log("----------------------");
+//     console.log(randomCardsReadyForEvaluation);
+//     console.log("----------------------");
+// }
 
 
 
@@ -35,9 +27,10 @@ function getPokerDeck() {
         let diamonds = "♦";
         let hearts = "♥";
         let spades = "♠";
-        if (i == 1) {
-            deck.push({ value: "A", suit: clubs }, { value: "A", suit: diamonds }, { value: "A", suit: hearts }, { value: "A", suit: spades });
-        } else if (i == 11) {
+        // if (i == 1) {
+        //     deck.push({ value: "A", suit: clubs }, { value: "A", suit: diamonds }, { value: "A", suit: hearts }, { value: "A", suit: spades });
+        // } else 
+        if (i == 11) {
             deck.push({ value: "J", suit: clubs }, { value: "J", suit: diamonds }, { value: "J", suit: hearts }, { value: "J", suit: spades });
         } else if (i == 12) {
             deck.push({ value: "Q", suit: clubs }, { value: "Q", suit: diamonds }, { value: "Q", suit: hearts }, { value: "Q", suit: spades });
@@ -48,16 +41,7 @@ function getPokerDeck() {
     return deck;
 }
 
-///////////////////////////////
-function printDeck(cards) {
-    var result = "";
-    for (card of cards) {
-        result += card.value + card.suit + " ";
-    }
-    console.log(cards.length + " " + result);
-}
-
-//This function uses the algorithm of fisherYates
+//This function shuffles the deck using the Fischer-Yates algorithm
 function shuffle(deck) {
     var i, j, k;
     for (i = deck.length - 1; i > 0; i--) {
@@ -69,26 +53,27 @@ function shuffle(deck) {
 }
 
 // This function returns the hand of cards:
-function getRemainingDeck(deck) {
-    while (deck.length >= 5) {
-        deck.splice(0,5);   
-        return deck;
-    }
-   
-}
+// function getRemainingDeck(deck) {
+//     var counter = deck.length;
+//     if (counter >= 2) {
+//         deck.splice(0,5);
+//         console.log("OKKIO " + counter);   
+//         return deck;
+//     } 
+//}
 
-//This function transforms card figures into integers
-function figuresIntoIntegers(card) {
-    switch (card.value) {
-        case "A":
-            return card.value = 1;
-        case "J":
-            return card.value = 11;
-        case "Q":
-            return card.value = 12;
-        case "K":
-            return card.value = 13;
-        default:
-            return card.value;
-    }
-}
+
+// function cardsDeckLimit(deck) {
+//     var counter = deck.length;
+//     console.log("Attenzione mancano " + deck.length + " carte");
+//     return counter > 7;
+// }
+
+// function cardsHandFromRemaningDeck(deck) {
+//     if (cardsDeckLimit(deck)) {
+//         console.log(cardsDeckLimit(deck))
+//         return deck.splice(0,5);
+//     } else return deck.splice(0,0);
+// }
+
+
