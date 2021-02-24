2262e5211ca42f540359eda8a3335c66a74d929f
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-02-24 05:48:36 +0100

    [UPDATE] randomCardsInInputField() in print_on_the_screen.js

--- assets/scripts/print_on_the_screen.js
+++ assets/scripts/print_on_the_screen.js
@@ -7,16 +7,16 @@ function resetPrintingScreen() {
 }
 
 function randomCardsInInputField(cards) {
-    document.getElementById("card_1").value = cards[0][0]
-    document.getElementById("card_suit_1").value = cards[0][1]
-    document.getElementById("card_2").value = cards[1][0]
-    document.getElementById("card_suit_2").value = cards[1][1]
-    document.getElementById("card_3").value = cards[2][0]
-    document.getElementById("card_suit_3").value = cards[2][1]
-    document.getElementById("card_4").value = cards[3][0]
-    document.getElementById("card_suit_4").value = cards[3][1]
-    document.getElementById("card_5").value = cards[4][0]
-    document.getElementById("card_suit_5").value = cards[4][1]
+    document.getElementById("card_1").value = cards[0].value;
+    document.getElementById("card_suit_1").value = cards[0].suit;
+    document.getElementById("card_2").value = cards[1].value;
+    document.getElementById("card_suit_2").value = cards[1].suit;
+    document.getElementById("card_3").value = cards[2].value;
+    document.getElementById("card_suit_3").value = cards[2].suit;
+    document.getElementById("card_4").value = cards[3].value;
+    document.getElementById("card_suit_4").value = cards[3].suit;
+    document.getElementById("card_5").value = cards[4].value;
+    document.getElementById("card_suit_5").value = cards[4].suit;
 }
 
 function resetValueInputField() {
@@ -27,3 +27,7 @@ function resetValueInputField() {
     document.getElementById("card_5").value = "";
 }
 
+function printingCounter(text) {
+    document.getElementById("counter_text").innerHTML = "Cards " + text.length + "/ 52";
+        
+}
