a86fbd4ff74a66638b08ba124cfeee005ef6f011
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-02-25 12:36:00 +0100

    [FIX] function displayRandomCards(), startRandom(), displayYourCards(), startUserInput() and the comments

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,18 @@
+//These functions when loading the page prepare the initial setup
 hideButton();
 resetValueInputField()
 
 var cardsDeck;
 var randomCardsHand;
 var userCardsHand;
+var textScore;
 
-
+//This function is invoked by the user pressing the key. 
+//Sets up the screen to display random card combinations.
+//Generate a 52-card deck, shuffle it, and subtract the top five cards from it, 
+//and print the number of remaining cards.
+//The hand of cards is printed on the screen, on the console, 
+//is analyzed and the score is printed
 function displayRandomCards() {
     resetValueInputField();
     resetPrintingScreen()
@@ -20,65 +27,63 @@ function displayRandomCards() {
     printingCardsConsole(randomCardsHand);
     printingCounter(cardsDeck);
     printingCardsConsole(cardsDeck);
+    textScore = evaluateCardsHand(randomCardsHand);
+    printingScreen(textScore);
+    console.log(textScore);
+    console.log(getCardsValues(randomCardsHand));
     console.log("-----------------------");
-    
-    
-   
-    //console.log(randomCardsReadyForEvaluation);
-    // var textScore = evaluaterandomCardsHand(randomCardsReadyForEvaluation);
-    // cardsToEvaluatePrintngConsole(randomCardsReadyForEvaluation, textScore);
-    // printingScreen(textScore);
 }
 
-function displayYourCards() {
-    resetValueInputField();
-    resetPrintingScreen();
-    document.getElementById("btn_user_cards").setAttribute("class", "display_block red")
-    document.getElementById("btn_random_cards").setAttribute("class", "display_none")
-}
-
+//This function is invoked by the user pressing the key.
+//This function takes 5 cards from the deck until there are fewer than 5 left.
+//When the deck is finished, on a further press, the deck is reset.
+//The cards are sold printed on the screen and on the console, 
+//they are analyzed and the result printed on the screen.
 function startRandom() {
-    // var randomCardsHand = getrandomCardsHand(cardsDeck);
-    
-    // randomCardsHandPrintingConsole(randomCardsHand);
-    // getCardsValues(randomCardsHand);
-    
     switch (cardsDeck.length > 5) {
         case true:
             randomCardsHand = cardsDeck.splice(0, 5);
             randomCardsInInputField(randomCardsHand);
             printingCounter(cardsDeck);
             printingCardsConsole(randomCardsHand);
-            printingCardsConsole(cardsDeck); 
-            console.log("----------------------")
+            printingCardsConsole(cardsDeck);
+            textScore = evaluateCardsHand(randomCardsHand);
+            printingScreen(textScore);
+            console.log(textScore);
+            console.log(getCardsValues(randomCardsHand));
+            console.log("-----------------------");
             break;
         case false:
             displayRandomCards()
     }
-    
-    
-    
-    
-    // var textScore = evaluaterandomCardsHand(randomCardsReadyForEvaluation);
-    //cardsToEvaluatePrintngConsole(randomCardsReadyForEvaluation, textScore);
-    //printingScreen(textScore);
 }
 
+//This function is invoked by the user pressing the key. 
+//Sets up the screen to display user card combinations.
+function displayYourCards() {
+    resetValueInputField();
+    resetPrintingScreen();
+    resetPrintingCounter()
+    document.getElementById("btn_user_cards").setAttribute("class", "display_block red")
+    document.getElementById("btn_random_cards").setAttribute("class", "display_none")
+}
+
+//This function is invoked by the user pressing the key.
+//This function analyzes the hand of cards chosen by the user, 
+//analyzes it and returns the score
 function startUserInput() {
     userCardsHand = getUserCardsHand()
     printingCardsConsole(userCardsHand);
-    //cardsValuesIntoIntegers(userCardsHand);
     console.log(userCardsHand);
-    console.log("----------------------")
-    
-    var textScore = evaluateCardsHand(userCardsHand);
-    // cardsToEvaluatePrintngConsole(userCardsReadyForEvaluation, textScore);
+    textScore = evaluateCardsHand(userCardsHand);
     printingScreen(textScore);
+    console.log(textScore);
+    console.log(getCardsValues(userCardsHand));
+    console.log("----------------------");
 }
 
+//This function hides the buttons that are not needed in the initial setup
 function hideButton() {
     document.getElementById("btn_user_cards").setAttribute("class", "display_none")
     document.getElementById("btn_random_cards").setAttribute("class", "display_none")
-
 }
-
