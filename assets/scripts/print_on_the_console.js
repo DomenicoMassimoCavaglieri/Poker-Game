7d20fb0c76c3f8fa51d8cd8eb852c14173dd6f35
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-02-25 12:38:39 +0100

    [UPDATE] Comments. Removed unused functions

--- assets/scripts/print_on_the_console.js
+++ assets/scripts/print_on_the_console.js
@@ -1,17 +1,8 @@
+//This function prints the cards on the console (decks or hands of cards)
 function printingCardsConsole(cards) {
     var result = "";
     for (card of cards) {
         result += card.value + card.suit + " ";
     }
     console.log(cards.length + " Cards: " + result);
-}
-
-function cardsHandPrintingConsole(hand) {
-    console.log("Your cards:" + " " + hand);
-}
-
-function cardsToEvaluatePrintngConsole(deckReadyForEvaluation, textScore) {
-    console.log("Cards ready for eveluation:" + " " + deckReadyForEvaluation);
-    console.log(textScore);
-    console.log("-------------------------------------")
 }
