17f172cd40482cbab45a548d7f543a46828044ec
Domenico Massimo Cavaglieri <domenicomassimo.cavaglieri@gmail.com>
2021-02-26 19:35:11 +0100

    [FIX] In user_cards.js indentation

--- assets/scripts/user_cards.js
+++ assets/scripts/user_cards.js
@@ -1,20 +1,30 @@
 //This function creates a multidimensional array with the cards chosen by the user
 function getUserCardsHand() {
     let userHand = [];
-    userHand.push({value: document.getElementById("card_1").value,
-    suit: document.getElementById("card_suit_1").value});
+    userHand.push({
+        value: document.getElementById("card_1").value,
+        suit: document.getElementById("card_suit_1").value
+    });
 
-    userHand.push({value: document.getElementById("card_2").value,
-    suit: document.getElementById("card_suit_2").value});
+    userHand.push({
+        value: document.getElementById("card_2").value,
+        suit: document.getElementById("card_suit_2").value
+    });
 
-    userHand.push({value: document.getElementById("card_3").value,
-    suit:document.getElementById("card_suit_3").value});
-    
-    userHand.push({value: document.getElementById("card_4").value,
-    suit:document.getElementById("card_suit_4").value});
-    
-    userHand.push({value: document.getElementById("card_5").value,
-    suit:document.getElementById("card_suit_5").value});
-    
+    userHand.push({
+        value: document.getElementById("card_3").value,
+        suit: document.getElementById("card_suit_3").value
+    });
+
+    userHand.push({
+        value: document.getElementById("card_4").value,
+        suit: document.getElementById("card_suit_4").value
+    });
+
+    userHand.push({
+        value: document.getElementById("card_5").value,
+        suit: document.getElementById("card_suit_5").value
+    });
+
     return userHand;
 }
